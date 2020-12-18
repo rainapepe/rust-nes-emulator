b9dd9e96c926f15e1119ed48b1116887945531bd
Raina Pepe <raina.pepe@jbtec.com.br>
2020-12-18 01:50:32 -0300

    create hex convertion

--- src/cpu/helpers.rs
+++ src/cpu/helpers.rs
@@ -12,8 +12,27 @@ use super::Cpu6502;
 //         s[i] = "0123456789ABCDEF"[n & 0xF];
 //     return s;
 // };
-fn to_hex(n: u32, d: u8) -> String {
-    let test = String::with_capacity(d as usize);
+
+const HEX_TABLE: &str = "0123456789ABCDEF";
+
+fn to_hex(value: u32, hex_size: u8) -> String {
+    let mut temp_value = value;
+    let mut text = Vec::<u8>::new();
+
+    // inicia o array com o tamanho do hex_size com zeros
+    text.resize(hex_size as usize, 0);
+
+    // começar do ultimo caracter para o primeiro
+    for i in (0..hex_size).rev() {
+        // identifica o caracter hex correpondente aos ultimos 4 bits
+        let c = HEX_TABLE.as_bytes()[(temp_value & 0xF) as usize];
+        text[i as usize] = c;
+
+        // mover 4 bits para a direita para poder achar o valor do proximos 4 bits
+        temp_value >>= 4;
+    }
+
+    String::from_utf8(text).unwrap()
 }
 
 impl Cpu6502 {
@@ -30,5 +49,14 @@ impl Cpu6502 {
         let value: u8 = 0;
         let lo: u8 = 0;
         let hi: u8 = 0;
+
+        // let mapLines
+        let mut line_addr: u16 = 0;
+
+        while addr <= stop as u32 {
+            line_addr = addr as u16;
+
+            let s_inst = format!("${}: ", to_hex(addr, 4));
+        }
     }
 }
